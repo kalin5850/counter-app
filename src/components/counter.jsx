667fc51d11f938c05e2a65fc40eb1f2b68e4a37d
kail <kail@tvws.com.tw>
2020-10-15 22:55:27 +0800

    add clssName into element

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,15 +4,13 @@ class Counter extends Component {
   // state is obj containing any data
   state = {
     count: 0,
-    imageUrl: "https://picsum.photos/200",
   };
 
   render() {
     return (
       <div>
-        <img src={this.state.imageUrl} alt="" />
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
