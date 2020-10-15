c5d17e33bbc71850968e4cb2d0efec0393ed4b03
kail <kail@tvws.com.tw>
2020-10-15 21:35:25 +0800

    add Counter comp

--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/counter';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Counter />, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
