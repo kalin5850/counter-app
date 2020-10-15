c6f84679a938fa8c56ae1938b8f8b0d2c6028227
kail <kail@tvws.com.tw>
2020-10-15 23:39:22 +0800

    add event handler

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -20,11 +20,20 @@ class Counter extends Component {
     );
   }
 
+  handleIncrement() {
+    console.log("Increment Clicked");
+  }
+
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
         {this.renderTags()}
       </div>
     );
